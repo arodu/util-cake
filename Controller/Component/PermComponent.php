43d31c10196b5be3be03b7357439f0bb187783d1
Alberto Rodriguez <arod.unerg@gmail.com>
2016-06-12 17:23:22 -0400

    PermComponent

--- Controller/Component/PermComponent.php
+++ Controller/Component/PermComponent.php
@@ -2,6 +2,12 @@
 App::uses('Component', 'Controller');
 class PermComponent extends Component {
 
+  private $userProfiles = array();
+  private $user_id = null;
+
+  private $User = null;
+  private $Profile = null;
+
   public $settings = array(
     'profile_type' => 'single',  // single o multiple
     'userModel' => 'User',
@@ -20,10 +26,26 @@ class PermComponent extends Component {
     $this->settings['userRoot'] = ( Configure::read('debug') > 0 ? true : $this->settings['userRoot'] );
     //$this->Controller->helpers['Permiso'] = array('component'=>$this); // <-- Cargar helper Permiso
 
+    $this->_loadModels();
+
     //$this->fileConfig();
     parent::__construct($collection, $this->settings);
   }
 
+  private function _loadModels(){
+    App::uses($this->settings['userModel'], 'Model');
+		$this->User = new $this->settings['userModel']();
 
+    App::uses($this->settings['profileModel'], 'Model');
+		$this->Profile = new $this->settings['profileModel']();
+  }
+
+  private function getUserProfiles($user_id){
+    if($this->settings['profile_type'] == 'single'){
+
+    }
+
+
+  }
 
 }
