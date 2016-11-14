82f18735cc5972defeda7b62bec7375c141e2852
Alberto Rodriguez <arod.unerg@gmail.com>
2016-11-14 18:40:15 -0400

    minor fix

--- Controller/Component/PermitComponent.php
+++ Controller/Component/PermitComponent.php
@@ -1,34 +1,34 @@
 <?php
 App::uses('Component', 'Controller');
 class PermitComponent extends Component {
-
+  
   public $settings = array(
     'config' => array(
-      'file' => 'permisos',
-      'name' => 'Permisos',
+      'file' => 'permits',
+      'name' => 'Permits',
     ),
-
+    
     'userModel' => 'User',
     'profileModel' => 'Profile',
-
+    
     'defaultResponse' => false,   // false,  loged,  public
     'userRoot' => false,
     'reload' => false,
     'errorMessage' => '',
   );
-
+  
   public $components = array('Auth','Session');
-
+  
   public function __construct(ComponentCollection $collection, $settings = array()) {
     $this->settings = array_replace_recursive($this->settings, (array)$settings);
     $this->controller = $collection->getController();
     $this->settings['userRoot'] = ( Configure::read('debug') > 0 ? true : $this->settings['userRoot'] );
-
+    
     $this->controller->helpers[] = 'UtilCake.Permit'; // <-- Cargar helper UtilCake.Permit
-
+    
     parent::__construct($collection, $this->settings);
   }
-
+  
   public function user($data = null){
     $this->userProfile = $this->loadUserProfile();
     if($data == null){
@@ -37,7 +37,7 @@ class PermitComponent extends Component {
       return ( isset($this->userProfile[$data]) ? $this->userProfile[$data] : false );
     }
   }
-
+  
   public function isAuthorized($current = null, $showError = true){
     $authorized = $this->_authorized($current);
     if($showError){
@@ -50,11 +50,11 @@ class PermitComponent extends Component {
     }
     return $authorized;
   }
-
+  
   public function hasPermission($current = null){
     return $this->_authorized($current);
   }
-
+  
   protected function _authorized($current = null){
     if($current == null){
       $current = array(
@@ -62,27 +62,27 @@ class PermitComponent extends Component {
         'action'=>$this->controller->params['action']
       );
     }
-
+    
     $this->userProfile = $this->loadUserProfile();
     $this->fileConfig = $this->loadFileConfig();
-
+    
     if($this->settings['userRoot']===true  &&  (isset($this->userProfile['root'])  &&  $this->userProfile['root']===true )){
       // El usuario es "root", y tiene acceso a todo por defecto
       return true;
     }
-
+    
     if($actionPermit = $this->getActionPermit($current)){
       if($actionPermit == 'public' or in_array('public', (array)$actionPermit)){
         // El permiso de la accion es "public", y cualquiera tiene acceso
         $this->controller->Auth->allow($current['action']);
         return true;
       }
-
+      
       if(($actionPermit == 'loged' or in_array('loged', (array)$actionPermit)) && $this->Auth->user('id')){
         // El permiso de la accion es "loged", y tienen acceso solo los usuarios logueados
         return true;
       }
-
+      
       if(is_array($actionPermit)){
         foreach ($actionPermit as $allowed_profile) {
           if(isset($this->userProfile[$allowed_profile]) and $this->userProfile[$allowed_profile]){
@@ -97,7 +97,7 @@ class PermitComponent extends Component {
     }
     return false;
   }
-
+  
   public function getActionPermit($current){
     $this->fileConfig = $this->loadFileConfig();
     if(isset($this->fileConfig[$current['controller']][$current['action']])){
@@ -111,16 +111,16 @@ class PermitComponent extends Component {
       return $actionPermit;
     }
   }
-
+  
   public function reloadPermits(){
     $this->fileConfig = $this->loadFileConfig(true);
     $this->loadUserProfile(true);
   }
-
+  
   private function loadFileConfig($reload = false){
     if($this->settings['reload'] or $reload){
       Configure::load($this->settings['config']['file']);
-  		$fileConfig = Configure::read($this->settings['config']['name']); // Leer Arreglo de Permisos
+      $fileConfig = Configure::read($this->settings['config']['name']); // Leer Arreglo de Permisos
       $this->Session->write('Permit.fileConfig', $fileConfig);
     }else{
       if( !($fileConfig = $this->Session->read('Permit.fileConfig')) ){
@@ -128,20 +128,20 @@ class PermitComponent extends Component {
       }
     }
     return $fileConfig;
-	}
-
+  }
+  
   private function loadUserProfile($reload = false){
-
+    
     if(!$this->Auth->user()) return array();
-
+    
     if($this->settings['reload'] or $reload){
       App::uses($this->settings['userModel'], 'Model');
       $userModel = new $this->settings['userModel']();
       $userModel->Behaviors->load('Containable');
-
+      
       App::uses($this->settings['profileModel'], 'Model');
       $profileModel = new $this->settings['profileModel']();
-
+      
       $user = $userModel->find('first',array(
         'conditions'=>array($userModel->alias.'.id' => $this->Auth->user('id')),
         'contain'=>array(
@@ -157,18 +157,18 @@ class PermitComponent extends Component {
     }
     return $userProfile;
   }
-
+  
   public function recreateProfile($arrayProfile = array()){
-		$aux = array();
-		foreach($arrayProfile as $profile) {
-			$aux[$profile['code']] = true;
-		}
-		if(!$this->settings['userRoot']){ unset($aux['root']); }
-		return $aux;
-	}
-
+    $aux = array();
+    foreach($arrayProfile as $profile) {
+      $aux[$profile['code']] = true;
+    }
+    if(!$this->settings['userRoot']){ unset($aux['root']); }
+    return $aux;
+  }
+  
   public function error(){
     throw new ForbiddenException($this->settings['errorMessage']);
   }
-
+  
 }
