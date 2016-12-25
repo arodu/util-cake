14f5e66ed978128f9a94a03d6c575eed4bd2eff8
Alberto Rodriguez <arod.unerg@gmail.com>
2016-12-25 13:00:38 -0400

    minor fix

---
+++ Model/Behavior/LogBehavior.php
@@ -0,0 +1,24 @@
+<?php
+App::uses('ModelBehavior', 'Model');
+class LogBehavior extends ModelBehavior {
+  
+  public $default = array(
+    'Model'=>'Log',
+  );
+  
+  public function setup(Model $model, $settings = array()) {
+    
+  }
+  
+  
+  public function set(){
+    
+    
+    
+    
+  }
+  
+  
+  
+}
+
